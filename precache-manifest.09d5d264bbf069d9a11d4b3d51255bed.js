80f4945ed5998735e35d71701f31c69efc895c9a
owais34 <owaishunter78@gmail.com>
2020-09-21 08:55:45 +0530

    Updates

---
+++ precache-manifest.09d5d264bbf069d9a11d4b3d51255bed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97dceef2466f6f17fda809bf0dcb535b",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "1c82ff58af356e05d68c",
+    "url": "/Robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "c5dcbaa766ccd48fccaf",
+    "url": "/Robofriends/static/css/main.e7754008.chunk.css"
+  },
+  {
+    "revision": "1c82ff58af356e05d68c",
+    "url": "/Robofriends/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5dcbaa766ccd48fccaf",
+    "url": "/Robofriends/static/js/main.840270d8.chunk.js"
+  },
+  {
+    "revision": "d53e560d43c7171e8016",
+    "url": "/Robofriends/static/js/runtime-main.e0286ea6.js"
+  }
+]);
